dae94ef65769751bad56ae1cc170bc8b4d6caa05
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-17 09:55:31 -0400

    Add Javadoc for AppRole and AppUser JPAs

--- src/main/java/edu/ccsu/cs492/kerberoschat/user/entity/AppRole.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/user/entity/AppRole.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+/**
+ * JPA class for user roles
+ *
+ * These roles are used in determining the access level of a user. The sets of these roles are not disjoint, ie an admin
+ * can have all the roles to represent they're an admin
+ */
 @Data
 @Entity
 @Table(name = "app_role", uniqueConstraints = {
@@ -12,10 +18,16 @@ import javax.persistence.*;
 })
 public class AppRole {
 
+    /**
+     * The id of a role
+     */
     @Id
     @Column(name = "role_id", nullable = false)
     Integer roleId;
 
+    /**
+     * A specific role for a user in the format ROLE_{ROLE_NAME}
+     */
     @Column(name = "role_name", length = 30, nullable = false)
     String roleName;
 }
