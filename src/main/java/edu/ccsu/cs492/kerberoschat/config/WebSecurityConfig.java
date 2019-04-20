958cacfc243cb075843737b9e48762e613fdaee8
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-19 22:31:29 -0400

    Update Security Config
    Update security config to allow blanket CORS access and disable csrf.
    Csrf specifically will have to be re-enabled later once its configured
    properly. CORS will need to be configured finely as well, but needs to
    be enabled so that the browser can send API requests.

--- src/main/java/edu/ccsu/cs492/kerberoschat/config/WebSecurityConfig.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/config/WebSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * Configuration for Password Encoder for Spring Security
      * <p>
-     * TODO: May need to return MessageDigestPasswordEncoder instead for client side authentication
+     * TODO: May need to return MessageDigestPasswordEncoder instead for faster client side authentication
      *
      * @return a new Pbkdf2PasswordEncoder for hashing user passwords
      */
@@ -27,5 +32,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
+        http.csrf().disable(); // TODO: Configure csrf to give a token to the browser on serving
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() { // TODO: Change blanket CORS enabling
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
