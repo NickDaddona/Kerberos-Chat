4c730c154db1cc375ca2f017a2437abb7c9ad4fe
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-26 13:47:09 -0400

    Add Keygeneration in KerberosService
    Add functionality to generate a SecretKey object containing a 256 bit
    AES key. The type of generation and securerandom used may have to change
    later depending on if this key can be easily transferred to the user as
    part of authentication.

--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosService.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosService.java
@@ -6,6 +6,10 @@ import edu.ccsu.cs492.kerberoschat.user.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
 @Service
 public class KerberosService {
     private final AppUserService appUserService;
@@ -24,5 +28,21 @@ public class KerberosService {
     public String getUserSalt(String userName) throws AppUserNotFoundException {
         AppUser user = appUserService.getUser(userName);
         return user.getPassword().substring(0, 16); // first 16 hex characters of hash are the salt
+    }
+
+    /**
+     * Generates a new AES-256 Key for a session that is being established
+     *
+     * @return a new SecretKey object that contains the generated key
+     */
+    public SecretKey generateSessionKey() {
+        try {
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(256); // TODO: may need to provide secure random different than one the class creates
+            return generator.generateKey();
+        } catch (NoSuchAlgorithmException e) { // TODO: Determine strategy for handling exception
+            e.printStackTrace();
+            return null;
+        }
     }
 }
