0b3c37a82dcf4e36a3ee584ba1b2ae2cb7128894
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-26 14:19:34 -0400

    Add Initial Authentication Controller

--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosRestController.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosRestController.java
@@ -1,6 +1,9 @@
 package edu.ccsu.cs492.kerberoschat.kerberos;
 
+import edu.ccsu.cs492.kerberoschat.kerberos.authenticator.Authenticator;
+import edu.ccsu.cs492.kerberoschat.user.entity.AppUser;
 import edu.ccsu.cs492.kerberoschat.user.exception.AppUserNotFoundException;
+import edu.ccsu.cs492.kerberoschat.user.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +21,12 @@ public class KerberosRestController {
 
     private final KerberosService kerberosService;
 
+    private final AppUserService appUserService;
+
     @Autowired
-    public KerberosRestController(KerberosService kerberosService) {
+    public KerberosRestController(KerberosService kerberosService, AppUserService appUserService) {
         this.kerberosService = kerberosService;
+        this.appUserService = appUserService;
     }
 
     /**
@@ -36,6 +42,28 @@ public class KerberosRestController {
             return new ResponseEntity<>(salt, HttpStatus.OK);
         } catch (AppUserNotFoundException e) { // Bad request status returned to not indicate if there is a user for that name
             return new ResponseEntity<>("bad request", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Starts the authentication process for Kerberos. If its successful, a user will recieve a new SessionAuthenticator
+     * with their TicketGrantingTicket and the session key they will be using with the KDC
+     *
+     * @param authenticator the user's authenticator
+     * @return a response containing a session authenticator if successful, a response indicating failure otherwise
+     */
+    @RequestMapping(value = "authenticate", method = RequestMethod.POST)
+    public ResponseEntity<String> getTimeStamp(@RequestBody Authenticator authenticator) {
+        try {
+            AppUser user = appUserService.getUser(authenticator.getUserName());
+            if (kerberosService.isTimestampValid(authenticator.getTimestamp())) {
+                return new ResponseEntity<>("Timestamp expired", HttpStatus.UNAUTHORIZED);
+            }
+            else {
+                return new ResponseEntity<>("Authenticated", HttpStatus.OK);
+            }
+        } catch (AppUserNotFoundException e) {
+            return new ResponseEntity<>("Failure to Authenticate", HttpStatus.UNAUTHORIZED);
         }
     }
 }
