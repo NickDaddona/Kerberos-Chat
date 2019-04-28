3c8322a383450fb5166ed73ad27787d9df83bab7
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-27 22:09:53 -0400

    Add Javadoc

--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/authenticator/Authenticator.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/authenticator/Authenticator.java
@@ -4,9 +4,18 @@ import lombok.Data;
 
 import java.util.Date;
 
+/**
+ * Used for a representation of a Request sent by a user for authentication
+ */
 @Data
 public class Authenticator {
+    /**
+     * The Name of the user that wishes to be authenticated
+     */
     private String userName;
 
+    /**
+     * A timestamp encrypted with the hash of the user's password
+     */
     private Date timestamp;
 }
--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/authenticator/SessionAuthenticator.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/authenticator/SessionAuthenticator.java
@@ -7,11 +7,22 @@ import lombok.NoArgsConstructor;
 
 import javax.crypto.SecretKey;
 
+/**
+ * A response object for when a user is authenticated, encrypted with the hash of the authenticated user's password
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SessionAuthenticator {
+
+    /**
+     * The session key the user will use to communicate with the KDC
+     * TODO: Should consider changing this to a string for interoperability with frontend
+     */
     private SecretKey sessionKey;
 
+    /**
+     * The ticket the user will send with every request to the KDC
+     */
     private TicketGrantingTicket ticketGrantingTicket;
 }
