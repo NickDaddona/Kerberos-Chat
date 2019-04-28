24030ab09eff048a40ea2bd44f51b26a481b47c9
Nick Daddona <ndaddona@my.ccsu.edu>
2019-04-27 22:21:20 -0400

    Update TGT Class
    Add functionality to check if a TGT is still valid and constructor to
    build a new TGT for the specified user and duration and with the
    specified session key. Add javadoc to TGT class

--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/ticket/TicketGrantingTicket.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/ticket/TicketGrantingTicket.java
@@ -1,23 +1,59 @@
 package edu.ccsu.cs492.kerberoschat.kerberos.ticket;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TicketGrantingTicket {
+    /**
+     * The name of the user the ticket was granted to
+     */
     private String username;
 
+    /**
+     * The time the ticket was issued
+     */
     private Date timeIssued;
 
+    /**
+     * The time the ticket is set to expire
+     */
     private Date expiryTime;
 
-    private long issuedTime;
+    /**
+     * How long the ticket is set to last
+     */
+    private long duration;
 
+    /**
+     * The session key for this ticket
+     * // TODO: Should consider changing this to a string to allow for easier encryption
+     */
     private SecretKey sessionKey;
+
+    /**
+     * Creates a new ticket granting ticket for the specified user with the specified duration
+     *
+     * @param username   the name of the user being granted the ticket
+     * @param duration   the duration in milliseconds the ticket will last
+     * @param sessionKey the key the user and the KDC will use for the session
+     */
+    public TicketGrantingTicket(String username, long duration, SecretKey sessionKey) {
+        this.username = username;
+        this.duration = duration;
+        this.timeIssued = new Date();
+        this.expiryTime = new Date(timeIssued.getTime() + duration);
+        this.sessionKey = sessionKey;
+    }
+
+    /**
+     * Determines if the ticket is still valid
+     *
+     * @return true if its valid, false otherwise
+     */
+    public boolean isValid() {
+        return timeIssued.getTime() + duration < expiryTime.getTime();
+    }
 }
