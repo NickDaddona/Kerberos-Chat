014288e4dea6d11e82f0d1cc0a033fa0d1d5b7c6
Nick Daddona <ndaddona@my.ccsu.edu>
2019-05-01 09:23:29 -0400

    Update KerberosRestController
    Update KerberosRestController to handle any exceptions caused by a
    malformed TicketGrantingTicket object and to ensure both the tgt and the
    session authenticator sent to the user contain the same session key.
    This session key was made to be a base64 string for interoperability
    between Spring and AngularJS.
    
    As of this point, there is no encryption for this process or checking of
    userroles.

--- src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosRestController.java
+++ src/main/java/edu/ccsu/cs492/kerberoschat/kerberos/KerberosRestController.java
@@ -1,6 +1,9 @@
 package edu.ccsu.cs492.kerberoschat.kerberos;
 
 import edu.ccsu.cs492.kerberoschat.kerberos.authenticator.Authenticator;
+import edu.ccsu.cs492.kerberoschat.kerberos.authenticator.SessionAuthenticator;
+import edu.ccsu.cs492.kerberoschat.kerberos.ticket.MalformedTGTException;
+import edu.ccsu.cs492.kerberoschat.kerberos.ticket.TicketGrantingTicket;
 import edu.ccsu.cs492.kerberoschat.user.entity.AppUser;
 import edu.ccsu.cs492.kerberoschat.user.exception.AppUserNotFoundException;
 import edu.ccsu.cs492.kerberoschat.user.service.AppUserService;
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 /**
  * Rest Controller related to Kerberos Authentication
@@ -53,16 +59,20 @@ public class KerberosRestController {
      * @return a response containing a session authenticator if successful, a response indicating failure otherwise
      */
     @RequestMapping(value = "authenticate", method = RequestMethod.POST)
-    public ResponseEntity<String> authenticateUser(@RequestBody Authenticator authenticator) {
+    public ResponseEntity<?> authenticateUser(@RequestBody Authenticator authenticator) {
         try {
             AppUser user = appUserService.getUser(authenticator.getUsername());
             if (kerberosService.isTimestampValid(authenticator.getTimestamp())) {
-                return new ResponseEntity<>("Authenticated", HttpStatus.OK);
+                SecretKey sessionKey = kerberosService.generateSessionKey(); // generate a new session key for the user
+                String encodedKey = Base64.getEncoder().encodeToString(sessionKey.getEncoded()); // encode as base64
+                TicketGrantingTicket TGT = kerberosService.createTGT(user, encodedKey); // create a TGT for the user
+                SessionAuthenticator sessionAuthenticator = new SessionAuthenticator(encodedKey, TGT); // package TGT
+                return new ResponseEntity<>(sessionAuthenticator, HttpStatus.OK); // send response
             }
-            else {
+            else { // timestamp is invalid or expired
                 return new ResponseEntity<>("Invalid Timestamp", HttpStatus.UNAUTHORIZED);
             }
-        } catch (AppUserNotFoundException e) {
+        } catch (AppUserNotFoundException | MalformedTGTException e) { // error building a tgt or no use was found in the database
             return new ResponseEntity<>("Failure to Authenticate", HttpStatus.UNAUTHORIZED);
         }
     }
