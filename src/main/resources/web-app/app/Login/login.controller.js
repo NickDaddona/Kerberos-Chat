a8e9064b5c9d37ddaf5882649d8b2532f7a66a4f
Nick Daddona <ndaddona@my.ccsu.edu>
2019-05-03 23:19:03 -0400

    Refactor loginController
    Reorder injected dependencies in loginController. Update comments.

--- src/main/resources/web-app/app/Login/login.controller.js
+++ src/main/resources/web-app/app/Login/login.controller.js
@@ -1,18 +1,20 @@
 'use strict';
 
 angular.module('login').controller('loginController', [
-    '$scope', 'loginService', '$location',
-    function ($scope, loginService, $location) {
-        //TODO: User cannot leave the login page until the TGT is sent and approved
+    'loginService', '$location', '$scope',
+    function (loginService, $location, $scope) {
         $scope.password = ""; // Stores the password
-
         $scope.user = ""; // This will save the user's name put into the username input box
 
-        // There is a better way to do this, find it; allow this does seem to work
+        /**
+         * Attempts to authenticate the user with the KDC, based on a set of promises provided by the loginService
+         * @param {string} username the username of the user
+         * @param {string} password the password of the user
+         */
         $scope.generateAuth = function (username, password) {
             loginService.getSalt(username).then(function (salt) { // get the user's salt
                 loginService.passHash(password, salt).then(function (hash) { // derive the user's password
-                    loginService.sendAuth(loginService.getAuthenticator(username, hash)).then(function() { // send a request for a TGT
+                    loginService.sendAuth(loginService.getAuthenticator(username, hash)).then(function () { // send a request for a TGT
                         $location.path("/message"); // authenticated, so redirect to messenger page
                     });
                 });
